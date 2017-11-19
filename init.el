5a7129add37e69007f2b1d2ce79eff59816d94a4
Sahil Sehwag <sehwagsahil002@gmail.com>
2017-11-19 10:08:29 +0530

    init.el: Changed Font and Font Size

--- init.el
+++ init.el
@@ -199,14 +199,18 @@
 	;UI
 		(menu-bar-mode -1)
 	;FONT
-		(set-default-font "InconsolataForPowerline NF-14")
+		(set-default-font "Inconsolata Nerd Font")
+		(set-face-attribute 'default nil :height 180)
 ;NOT TO TOUCH
 	(custom-set-variables
-		;; custom-set-variables was added by Custom.
-		;; If you edit it by hand, you could mess it up, so be careful.
-		;; Your init file should contain only one such instance.
-		;; If there is more than one, they won't work right.
-		'(custom-safe-themes
-			(quote ("b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default)))
-		'(package-selected-packages (quote (##))))
-
+	 ;; custom-set-variables was added by Custom.
+	 ;; If you edit it by hand, you could mess it up, so be careful.
+	 ;; Your init file should contain only one such instance.
+	 ;; If there is more than one, they won't work right.
+	 '(package-selected-packages (quote (fzf org-edna))))
+	(custom-set-faces
+	 ;; custom-set-faces was added by Custom.
+	 ;; If you edit it by hand, you could mess it up, so be careful.
+	 ;; Your init file should contain only one such instance.
+	 ;; If there is more than one, they won't work right.
+	 )
