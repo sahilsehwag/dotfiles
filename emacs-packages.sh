5787ad1666588b5bf1526246abee25ba2652311a
Sahil Sehwag <sehwagsahil002@gmail.com>
2017-11-21 17:00:30 +0530

    emacs-packages.sh: Added dashboard, molokai and few evil plugins. Reorganized the file

--- emacs-packages.sh
+++ emacs-packages.sh
@@ -1,30 +1,50 @@
-git clone https://github.com/emacs-evil/evil/                      ~/.emacs.d/packages/evil
-git clone https://github.com/gabesoft/evil-mc/                     ~/.emacs.d/packages/evil-mc
-git clone https://github.com/cofi/evil-leader/                     ~/.emacs.d/packages/evil-leader
-git clone https://github.com/emacs-evil/evil-surround/             ~/.emacs.d/packages/evil-surround
-git clone https://github.com/redguardtoo/evil-nerd-commenter/      ~/.emacs.d/packages/evil-nerd-commenter
-git clone https://github.com/TheBB/evil-indent-plus/               ~/.emacs.d/packages/evil-indent-plus
-git clone https://github.com/syl20bnr/evil-escape/                 ~/.emacs.d/packages/evil-escape
-git clone https://github.com/cofi/evil-numbers/                    ~/.emacs.d/packages/evil-numbers
-git clone https://github.com/noctuid/evil-textobj-column/          ~/.emacs.d/packages/evil-textobj-column
-git clone https://github.com/supermomonga/evil-textobj-entire/     ~/.emacs.d/packages/evil-textobj-entire
-git clone https://github.com/syohex/evil-textobj-line/             ~/.emacs.d/packages/evil-textobj-line
-git clone https://github.com/noctuid/evil-textobj-anyblock/        ~/.emacs.d/packages/evil-textobj-anyblock
-git clone https://github.com/redguardtoo/evil-matchit/             ~/.emacs.d/packages/evil-matchit
-git clone https://github.com/edkolev/evil-lion/                    ~/.emacs.d/packages/evil-lion
-git clone https://github.com/wcsmith/evil-args/                    ~/.emacs.d/packages/evil-args
-git clone https://github.com/7696122/evil-terminal-cursor-changer/ ~/.emacs.d/packages/evil-terminal-cursor-changer
-git clone https://github.com/alexmurray/evil-vimish-fold/          ~/.emacs.d/packages/evil-vimish-fold
-git clone https://github.com/mrkkrp/vimish-fold/                   ~/.emacs.d/packages/vimish-fold
-git clone https://github.com/noctuid/targets.el/                   ~/.emacs.d/packages/targets.el
-git clone https://github.com/winterTTr/ace-jump-mode/              ~/.emacs.d/packages/ace-jump-mode
-git clone https://github.com/milkypostman/powerline/               ~/.emacs.d/packages/powerline
-git clone https://github.com/jonathanchu/emacs-powerline/          ~/.emacs.d/packages/emacs-powerline
-git clone https://github.com/AnthonyDiGirolamo/airline-themes/     ~/.emacs.d/packages/airline-themes
-git clone https://github.com/gabesoft/evil-mc/                     ~/.emacs.d/packages/evil-mc
-git clone https://github.com/jwiegley/org-mode/               	   ~/.emacs.d/packages/org-mode
-git clone https://github.com/Somelauw/evil-org-mode/               ~/.emacs.d/packages/evil-org-mode
-git clone https://github.com/krisajenkins/evil-tabs/               ~/.emacs.d/packages/evil-tabs
-git clone https://github.com/edkolev/evil-goggles/                 ~/.emacs.d/packages/evil-goggles
-git clone https://github.com/tarao/evil-plugins/               	   ~/.emacs.d/packages/evil-plugins
-git clone https://github.com/dracula/emacs/                        ~/.emacs.d/themes
+#EVIL
+	git clone https://github.com/emacs-evil/evil/                      ~/.emacs.d/packages/evil
+	git clone https://github.com/cofi/evil-leader/                     ~/.emacs.d/packages/evil-leader
+	git clone https://github.com/syl20bnr/evil-escape/                 ~/.emacs.d/packages/evil-escape
+	git clone https://github.com/emacs-evil/evil-surround/             ~/.emacs.d/packages/evil-surround
+	git clone https://github.com/redguardtoo/evil-nerd-commenter/      ~/.emacs.d/packages/evil-nerd-commenter
+	git clone https://github.com/TheBB/evil-indent-plus/               ~/.emacs.d/packages/evil-indent-plus
+	git clone https://github.com/gabesoft/evil-mc/                     ~/.emacs.d/packages/evil-mc
+	git clone https://github.com/cofi/evil-numbers/                    ~/.emacs.d/packages/evil-numbers
+	git clone https://github.com/noctuid/evil-textobj-column/          ~/.emacs.d/packages/evil-textobj-column
+	git clone https://github.com/supermomonga/evil-textobj-entire/     ~/.emacs.d/packages/evil-textobj-entire
+	git clone https://github.com/syohex/evil-textobj-line/             ~/.emacs.d/packages/evil-textobj-line
+	git clone https://github.com/noctuid/evil-textobj-anyblock/        ~/.emacs.d/packages/evil-textobj-anyblock
+	git clone https://github.com/redguardtoo/evil-matchit/             ~/.emacs.d/packages/evil-matchit
+	git clone https://github.com/edkolev/evil-lion/                    ~/.emacs.d/packages/evil-lion
+	git clone https://github.com/wcsmith/evil-args/                    ~/.emacs.d/packages/evil-args
+	git clone https://github.com/7696122/evil-terminal-cursor-changer/ ~/.emacs.d/packages/evil-terminal-cursor-changer
+	git clone https://github.com/alexmurray/evil-vimish-fold/          ~/.emacs.d/packages/evil-vimish-fold
+	git clone https://github.com/Somelauw/evil-org-mode/               ~/.emacs.d/packages/evil-org-mode
+	git clone https://github.com/krisajenkins/evil-tabs/               ~/.emacs.d/packages/evil-tabs
+	git clone https://github.com/edkolev/evil-goggles/                 ~/.emacs.d/packages/evil-goggles
+	git clone https://github.com/tarao/evil-plugins/                   ~/.emacs.d/packages/evil-plugins
+	git clone https://github.com/Dewdrops/evil-exchange                ~/.emacs.d/packages/evil-exchange
+	git clone https://github.com/Dewdrops/evil-extra-operator          ~/.emacs.d/packages/evil-extra-operator
+	git clone https://github.com/bling/evil-jumper                     ~/.emacs.d/packages/evil-jumper
+	git clone https://github.com/bling/evil-visualstar                 ~/.emacs.d/packages/evil-visualstar
+	git clone https://github.com/hlissner/evil-snipe                   ~/.emacs.d/packages/evil-snipe
+	git clone https://github.com/noctuid/evil-textobj-column           ~/.emacs.d/packages/evil-textobj-column
+	git clone https://github.com/coldnew/linum-relative                ~/.emacs.d/packages/linum-relative
+#EMACS
+	git clone https://github.com/mrkkrp/vimish-fold/                   ~/.emacs.d/packages/vimish-fold
+	git clone https://github.com/winterTTr/ace-jump-mode/              ~/.emacs.d/packages/ace-jump-mode
+	git clone https://github.com/noctuid/targets.el/                   ~/.emacs.d/packages/targets.el
+#LANGUAGES
+	git clone https://github.com/jwiegley/org-mode/               	   ~/.emacs.d/packages/org-mode
+#LOOK & FEEL
+	git clone https://github.com/rakanalh/emacs-dashboard          ~/.emacs.d/packages/dashboard
+	git clone https://github.com/milkypostman/powerline/           ~/.emacs.d/packages/powerline
+	git clone https://github.com/Dewdrops/powerline		           ~/.emacs.d/packages/evil-powerline
+	git clone https://github.com/AnthonyDiGirolamo/airline-themes/ ~/.emacs.d/packages/airline-themes
+	git clone https://github.com/jonathanchu/emacs-powerline/      ~/.emacs.d/packages/emacs-powerline
+	git clone https://github.com/TheBB/spaceline                   ~/.emacs.d/packages/spaceline
+	#THEMES
+		curl https://raw.githubusercontent.com/hbin/molokai-theme/master/molokai-theme.el > ~/.emacs.d/themes/molokai-theme.el
+		curl https://raw.githubusercontent.com/dracula/emacs/master/dracula-theme.el      > ~/.emacs.d/themes/dracula-theme.el
+#DEPENDENCIES
+	git clone https://github.com/purcell/page-break-lines ~/.emacs.d/packages/page-break-lines
+	git clone https://github.com/emacs-evil/goto-chg      ~/.emacs.d/packages/goto-chg
+	git clone https://github.com/emacsmirror/undo-tree    ~/.emacs.d/packages/undo-tree
+	git clone https://github.com/magnars/dash.el    	  ~/.emacs.d/packages/dash
