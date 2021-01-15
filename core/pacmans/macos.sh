c90104ccda8300ddd4ec99a55e7e7f4e708ba951
Sahil Sehwag <sahilsehwag@sprinklr-macbook.local>
2021-01-15 22:57:16 +0530

    pacmans/macos: Implemented pacman specification for macos.sh

--- core/pacmans/macos.sh
+++ core/pacmans/macos.sh
@@ -1,50 +1,17 @@
 
-
-# ========
-# HOMEBREW
-# ========
-function brew::install() {
-	if [ $1 ]; then
-		brew install $1
-	else
-		echo "No package to install"
-	fi
-}
-
-function brew::uninstall() {
-	if [ $1 ]; then
-		brew uninstall $1
-	else
-		echo "No package to uninstall"
-	fi
-}
-
-
-# =============
-# HOMEBREW-CASK
-# =============
-function cask::install() {
-	if [ $1 ]; then
-		brew --cask install $1
-	else
-		echo "No package to install"
-	fi
-}
-
-function cask::uninstall() {
-	if [ $1 ]; then
-		brew --cask uninstall $1
-	else
-		echo "No package to uninstall"
-	fi
-}
-
-
 # =======
 # ALIASES
 # =======
-alias install="brew::install"
-alias uninstall="brew::uninstall"
+alias pacman.cmd="brew"
+alias pacman.find="brew info"
+alias pacman.add="brew install"
+alias pacman.remove="brew uninstall"
+alias pacman.list="brew info"
+alias pacman.show="brew info"
+alias pacman.update="brew upgrade"
+alias pacman.upgrade="brew update"
+alias pacman.add-mirror="brew tap"
+alias pacman.remove-mirror="brew untap"
 
-alias sinstall="cask::install"
-alias suninstall="cask::uninstall"
+alias pacman-alt.add="brew --cask install"
+alias pacman-alt.remove="brew --cask uninstall"
