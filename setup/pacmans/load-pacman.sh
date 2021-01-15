13c64171e6ba923ff700799012556c8085a840d6
Sahil Sehwag <sahilsehwag@sprinklr-macbook.local>
2021-01-15 23:08:27 +0530

    pacmans: Moved pacmans to setup

---
+++ setup/pacmans/load-pacman.sh
@@ -0,0 +1,10 @@
+
+source ../variables.sh
+
+if [[ meta.os =~ "mac" ]]; then
+	source ./macos.sh
+elif [[ meta.os =~ "ubuntu" ]]; then
+	source ./ubuntu.sh
+elif [[ meta.os =~ "windows" ]]; then
+	source ./windows.sh
+fi
