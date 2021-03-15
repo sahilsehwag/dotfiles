79470dbf31adf3943ee586b34749054a0fb7d817
Sahil Sehwag <sehwagsahil002@gmail.com>
2021-03-15 15:42:27 +0530

    Added dotfiles/vim/lspkind-nvim.lua

---
+++ dotfiles/vim/plugins/lsp/lspkind-nvim.lua
@@ -0,0 +1,27 @@
+-- פּ                 
+--   <>   	       ﱮ  塞       練蘒
+require('lspkind').init({
+  with_text = true,
+  symbol_map = {
+    Text = '  ',
+    Method = '  ',
+    Function = '  ',
+    Constructor = '  ',
+    Variable = '[]',
+    Class = '  ',
+    Interface = '  ',
+    Module = ' פּ ',
+    Property = '  ',
+    Unit = ' 塞 ',
+    Value = '  ',
+    Enum = ' 練',
+    Keyword = '  ',
+    Snippet = '  ',
+    Color = '  ',
+    File = '  ',
+    Folder = ' ﱮ ',
+    EnumMember = '  ',
+    Constant = '  ',
+    Struct = '  '
+  },
+})
