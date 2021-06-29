9bb61c4895533ba29cfb6b454bb3a1da3fdb0c4e
Sahil Sehwag <sehwagsahil002@gmail.com>
2021-06-29 13:59:26 +0530

    bugfix | vim | Fixed goto-preview

--- dotfiles/vim/plugins/editor/goto-preview.lua
+++ dotfiles/vim/plugins/editor/goto-preview.lua
@@ -1,21 +1,15 @@
 require('goto-preview').setup({
-	width = 120;
-	height = 15;
+	width = 180;
+	height = 30;
 	default_mappings = false;
-	lsp_configs = {
-		lua = {
-			get_config = function(data)
-				return data.targetUri,{ data.targetRange.start.line + 1, data.targetRange.start.character }
-			end
-		};
-		typescript = {
-			get_config = function(data)
-				return data.uri, { data.range.start.line + 1, data.range.start.character }
-			end
-		}
-	}
+	opacity = nil;
+	post_open_hook = nil;
+	debug = false;
 })
 
 vim.cmd [[nnoremap <leader>lpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>]]
 vim.cmd [[nnoremap <leader>lpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>]]
-vim.cmd [[nnoremap <leader>lP <cmd>lua require('goto-preview').close_all_win()<CR>]]
+vim.cmd [[nnoremap <leader>lpq <cmd>lua require('goto-preview').close_all_win()<CR>]]
+
+vim.cmd [[nnoremap gp <cmd>lua require('goto-preview').goto_preview_definition()<CR>]]
+vim.cmd [[nnoremap gP <cmd>lua require('goto-preview').goto_preview_implementation()<CR>]]
