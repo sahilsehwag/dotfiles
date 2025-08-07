ac36e3245a2261edc3251e7d0911ccd98f963241
Sahil Sehwag <sehwagsahil002@gmail.com>
2025-08-07 16:04:27 +0530

    aerospace: renamed config to aerospace.toml and added linking logic during install

--- packages/aerospace/install.sh
+++ packages/aerospace/install.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
+script_directory=$(F_getScriptDir "${BASH_SOURCE:-$0}")
+
 brew install --cask nikitabobko/tap/aerospace
+
+if ! F_isSoftlink "$HOME/.config/aerospace"; then
+	ln -sv "$script_directory/config/" "$HOME/.config/aerospace"
+fi
