acced5d70ce310f8146ec3d38fe1353ff47c5ab6
Sahil Sehwag <sehwagsahil002@gmail.com>
2025-08-08 18:12:20 +0530

    nvim(debugprint): added mappings and renamed copilot.lua to avoid name conflict

--- packages/nvim/configs/debugprint.lua
+++ packages/nvim/configs/debugprint.lua
@@ -1,6 +1,25 @@
 require("debugprint").setup()
 
-F.vim.nmap('g?d', function()
-		require'debugprint'.deleteprints()
-	end
-)
+vim.keymap.set("n", "g?d", function()
+	vim.cmd("Debugprint delete")
+end, {
+	desc = "[Debugprint] Delete debug print",
+})
+
+vim.keymap.set("n", "g?/", function()
+	vim.cmd("Debugprint search")
+end, {
+	desc = "[Debugprint] Search debug prints",
+})
+
+vim.keymap.set("n", "g?q", function()
+	vim.cmd("Debugprint quickfix")
+end, {
+	desc = "[Debugprint] Quickfix debug prints",
+})
+
+vim.keymap.set("n", "g?k", function()
+	vim.cmd("Debugprint commenttoggle")
+end, {
+	desc = "[Debugprint] Toggle comment on debug print",
+})
