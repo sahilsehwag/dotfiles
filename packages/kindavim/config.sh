9a9a81ffc23a07c77ab2a1db911e826df539069a
Sahil Sehwag <sehwagsahil002@gmail.com>
2025-08-12 20:26:46 +0530

    kindavim: updated config name

--- packages/kindavim/config.sh
+++ packages/kindavim/config.sh
@@ -2,5 +2,5 @@
 script_directory=$(F_getScriptDir ${BASH_SOURCE:-$0})
 
 if ! F_isSoftlink "$HOME/Documents/projects/personal/mani.yml"; then
-	ln -sv $script_directory/../config/kindavim.plist "$HOME/Library/Preferences/mo.com.sleeplessmind.kindaVim.plist"
+	ln -sv $script_directory/mo.com.sleeplessmind.kindaVim.plist "$HOME/Library/Preferences/mo.com.sleeplessmind.kindaVim.plist"
 fi
