7b11f0f31c3e3a092e4907eeb7ad71a8be368ea5
Sahil Sehwag <sehwagsahil002@gmail.com>
2025-08-15 03:43:38 +0530

    java: added java installation for mac

---
+++ packages/java/install.mac.sh
@@ -0,0 +1,2 @@
+brew install openjdk
+sudo ln -sfn $(brew --prefix openjdk)/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
--- packages/java/install.sh
+++ packages/java/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 script_directory=$(F_getScriptDir ${BASH_SOURCE:-$0})
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if F_isMac; then
 	source $script_directory/install.mac.sh
+else
+	F_logError "Java installation is only supported on macOS."
 fi
