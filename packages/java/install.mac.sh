386bd9728ddb3d3e1ecafe613fa28db7637c61e1
Sahil Sehwag <sehwagsahil002@gmail.com>
2025-08-15 04:16:04 +0530

    java: added sdkman, maven and gradle

--- packages/java/install.mac.sh
+++ packages/java/install.mac.sh
@@ -1,2 +1,5 @@
 brew install openjdk
+brew install maven
+brew install gradle
+
 sudo ln -sfn $(brew --prefix openjdk)/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
--- packages/java/install.sh
+++ packages/java/install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 script_directory=$(F_getScriptDir ${BASH_SOURCE:-$0})
 
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
 if F_isMac; then
 	source $script_directory/install.mac.sh
 else
