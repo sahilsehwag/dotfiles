ef99fec08b378449a151d01a2e1a0e5b7a6f1986
Sahil Sehwag <sehwagsahil002@gmail.com>
2026-05-23 23:13:55 +0530

    starship: updated timeout to 10 seconds

--- packages/starship/install.sh
+++ packages/starship/install.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+script_directory=$(F_getScriptDir ${BASH_SOURCE:-$0})
 
 F_pkg_install starship
+
+if ! F_isSoftlink "$HOME/.config/starship.toml"; then
+	ln -sv $script_directory/starship.toml $HOME/.config/starship.toml
+fi
