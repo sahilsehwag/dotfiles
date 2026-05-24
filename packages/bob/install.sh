e32b5fd98194a1457fd84a469e1e7cdc57be5270
Sahil Sehwag <sehwagsahil002@gmail.com>
2026-05-25 03:36:31 +0530

    nvim: fixes

--- packages/bob/install.sh
+++ packages/bob/install.sh
@@ -2,7 +2,6 @@
 cargo install --git https://github.com/MordechaiHadad/bob.git
 #cargo install bob-nvim
 
-
 bob install nightly
 bob install stable
 bob install 0.8.0
--- packages/nvim/install.sh
+++ packages/nvim/install.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+
 script_directory=$(F_getScriptDir "${BASH_SOURCE:-$0}")
-pip3 show neovim &>/dev/null || pip3 install neovim
-pip3 show pynvim &>/dev/null || pip3 install pynvim
+
+if F_isInstalled pipx; then
+	pipx show neovim &>/dev/null || pipx install neovim
+	pipx show pynvim &>/dev/null || pipx install pynvim
+else
+	pip3 show neovim &>/dev/null || pip3 install neovim
+	pip3 show pynvim &>/dev/null || pip3 install pynvim
+fi
+
 npm info neovim &>/dev/null  || npm install -g neovim
 
 source "$script_directory/scripts/install.plug.sh"
