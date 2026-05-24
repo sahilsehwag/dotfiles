3b4962c849a662f958ba701aa9e1e7628a510402
Sahil Sehwag <sehwagsahil002@gmail.com>
2026-05-24 23:56:11 +0530

    vifm: loading/setup fixes

--- packages/vifm/is_configed.sh
+++ packages/vifm/is_configed.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-F_isSoftlink "$HOME/.config/vifm" && return 0 || return 1
+F_isSoftlink "$HOME/.config/vifm" && [[ -d "$HOME/.config/vifm/vifm_devicons" ]] && return 0 || return 1
