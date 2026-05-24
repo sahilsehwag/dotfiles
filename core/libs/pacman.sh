da14664aacf4184a023f207d376146aa84c2cb05
Sahil Sehwag <sehwagsahil002@gmail.com>
2026-05-25 00:43:31 +0530

    pacman: added os field to prevent unncessary runs for not-available package managers

--- core/libs/pacman.sh
+++ core/libs/pacman.sh
@@ -2,8 +2,14 @@
 declare -A F__pacmans
 declare __F_idx=-1
 
+# os field matches OS_KERNEL from core/libs/os.sh:
+#   macos | linux | freebsd | openbsd | netbsd | windows | any
+
+# --- macOS ---
+
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='brew'
+F__pacmans[$__F_idx,'os']='macos'
 F__pacmans[$__F_idx,'install']='brew install'
 F__pacmans[$__F_idx,'uninstall']='brew uninstall'
 F__pacmans[$__F_idx,'update']='brew upgrade'
@@ -12,14 +18,18 @@ F__pacmans[$__F_idx,'show']='brew info'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='port'
+F__pacmans[$__F_idx,'os']='macos'
 F__pacmans[$__F_idx,'install']='port install'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
 F__pacmans[$__F_idx,'search']='false'
 F__pacmans[$__F_idx,'show']='false'
 
+# --- Windows ---
+
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='scoop'
+F__pacmans[$__F_idx,'os']='windows'
 F__pacmans[$__F_idx,'install']='scoop install'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
@@ -28,14 +38,18 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='choco'
+F__pacmans[$__F_idx,'os']='windows'
 F__pacmans[$__F_idx,'install']='choco install'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
 F__pacmans[$__F_idx,'search']='false'
 F__pacmans[$__F_idx,'show']='false'
 
+# --- Linux (Arch) ---
+
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='pacman'
+F__pacmans[$__F_idx,'os']='linux'
 F__pacmans[$__F_idx,'install']='pacman -Sy'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
@@ -44,22 +58,18 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='yay'
+F__pacmans[$__F_idx,'os']='linux'
 F__pacmans[$__F_idx,'install']='yay -Sy'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
 F__pacmans[$__F_idx,'search']='false'
 F__pacmans[$__F_idx,'show']='false'
 
-#let __F_idx++
-#F__pacmans[$__F_idx,'cmd']='abs'
-#F__pacmans[$__F_idx,'install']='abs install'
-#F__pacmans[$__F_idx,'uninstall']='false'
-#F__pacmans[$__F_idx,'update']='false'
-#F__pacmans[$__F_idx,'search']='false'
-#F__pacmans[$__F_idx,'show']='false'
+# --- Linux (Debian) ---
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='apt'
+F__pacmans[$__F_idx,'os']='linux'
 F__pacmans[$__F_idx,'install']='apt install -y'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
@@ -68,6 +78,7 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='apt-get'
+F__pacmans[$__F_idx,'os']='linux'
 F__pacmans[$__F_idx,'install']='apt-get install -y'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
@@ -76,15 +87,19 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='dpkg'
-F__pacmans[$__F_idx,'install']='dpkg install'
+F__pacmans[$__F_idx,'os']='linux'
+F__pacmans[$__F_idx,'install']='dpkg -i'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
 F__pacmans[$__F_idx,'search']='false'
 F__pacmans[$__F_idx,'show']='false'
 
+# --- Linux (RHEL/Fedora) ---
+
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='dnf'
-F__pacmans[$__F_idx,'install']='dnf install'
+F__pacmans[$__F_idx,'os']='linux'
+F__pacmans[$__F_idx,'install']='dnf install -y'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
 F__pacmans[$__F_idx,'search']='false'
@@ -92,70 +107,94 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='yum'
+F__pacmans[$__F_idx,'os']='linux'
 F__pacmans[$__F_idx,'install']='yum install -y'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
 F__pacmans[$__F_idx,'search']='false'
 F__pacmans[$__F_idx,'show']='false'
 
-#let __F_idx++
-#F__pacmans[$__F_idx,'cmd']='urpmi'
-#F__pacmans[$__F_idx,'install']='urpmi install'
-#F__pacmans[$__F_idx,'uninstall']='false'
-#F__pacmans[$__F_idx,'update']='false'
-#F__pacmans[$__F_idx,'search']='false'
-#F__pacmans[$__F_idx,'show']='false'
-
-#let __F_idx++
-#F__pacmans[$__F_idx,'cmd']='zypp'
-#F__pacmans[$__F_idx,'install']='zypp install'
-#F__pacmans[$__F_idx,'uninstall']='false'
-#F__pacmans[$__F_idx,'update']='false'
-#F__pacmans[$__F_idx,'search']='false'
-#F__pacmans[$__F_idx,'show']='false'
-
-#let __F_idx++
-#F__pacmans[$__F_idx,'cmd']='up2date'
-#F__pacmans[$__F_idx,'install']='up2date install'
-#F__pacmans[$__F_idx,'uninstall']='false'
-#F__pacmans[$__F_idx,'update']='false'
-#F__pacmans[$__F_idx,'search']='false'
-#F__pacmans[$__F_idx,'show']='false'
-
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='rpm'
-F__pacmans[$__F_idx,'install']='rpm install'
+F__pacmans[$__F_idx,'os']='linux'
+F__pacmans[$__F_idx,'install']='rpm -i'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
 F__pacmans[$__F_idx,'search']='false'
 F__pacmans[$__F_idx,'show']='false'
 
+# --- Linux (SUSE) ---
+
 let __F_idx++
-F__pacmans[$__F_idx,'cmd']='pkg'
-F__pacmans[$__F_idx,'install']='pkg install'
+F__pacmans[$__F_idx,'cmd']='zypper'
+F__pacmans[$__F_idx,'os']='linux'
+F__pacmans[$__F_idx,'install']='zypper install -y'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
 F__pacmans[$__F_idx,'search']='false'
 F__pacmans[$__F_idx,'show']='false'
 
+# --- Linux (Alpine) ---
+
 let __F_idx++
-F__pacmans[$__F_idx,'cmd']='pkgin'
-F__pacmans[$__F_idx,'install']='pkgin install'
+F__pacmans[$__F_idx,'cmd']='apk'
+F__pacmans[$__F_idx,'os']='linux'
+F__pacmans[$__F_idx,'install']='apk add'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
 F__pacmans[$__F_idx,'search']='false'
 F__pacmans[$__F_idx,'show']='false'
+
+# --- Linux (Void) ---
+
+let __F_idx++
+F__pacmans[$__F_idx,'cmd']='xbps-install'
+F__pacmans[$__F_idx,'os']='linux'
+F__pacmans[$__F_idx,'install']='xbps-install -y'
+F__pacmans[$__F_idx,'uninstall']='false'
+F__pacmans[$__F_idx,'update']='false'
+F__pacmans[$__F_idx,'search']='false'
+F__pacmans[$__F_idx,'show']='false'
+
+# --- Linux (Gentoo) ---
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='emerge'
+F__pacmans[$__F_idx,'os']='linux'
 F__pacmans[$__F_idx,'install']='emerge -av'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
 F__pacmans[$__F_idx,'search']='false'
 F__pacmans[$__F_idx,'show']='false'
 
+# --- FreeBSD ---
+
+let __F_idx++
+F__pacmans[$__F_idx,'cmd']='pkg'
+F__pacmans[$__F_idx,'os']='freebsd'
+F__pacmans[$__F_idx,'install']='pkg install -y'
+F__pacmans[$__F_idx,'uninstall']='false'
+F__pacmans[$__F_idx,'update']='false'
+F__pacmans[$__F_idx,'search']='false'
+F__pacmans[$__F_idx,'show']='false'
+
+# --- NetBSD ---
+
+let __F_idx++
+F__pacmans[$__F_idx,'cmd']='pkgin'
+F__pacmans[$__F_idx,'os']='netbsd'
+F__pacmans[$__F_idx,'install']='pkgin install'
+F__pacmans[$__F_idx,'uninstall']='false'
+F__pacmans[$__F_idx,'update']='false'
+F__pacmans[$__F_idx,'search']='false'
+F__pacmans[$__F_idx,'show']='false'
+
+# --- Cross-platform (language toolchains) ---
+# These are installed on top of any OS, so os=any skips the OS gate.
+
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='nix-env'
+F__pacmans[$__F_idx,'os']='any'
 F__pacmans[$__F_idx,'install']='nix-env -i'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
@@ -163,23 +202,8 @@ F__pacmans[$__F_idx,'search']='false'
 F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
-F__pacmans[$__F_idx,'cmd']='apk'
-F__pacmans[$__F_idx,'install']='apk add'
-F__pacmans[$__F_idx,'uninstall']='false'
-F__pacmans[$__F_idx,'update']='false'
-F__pacmans[$__F_idx,'search']='false'
-F__pacmans[$__F_idx,'show']='false'
-
-let __F_idx++
-F__pacmans[$__F_idx,'cmd']='xbps'
-F__pacmans[$__F_idx,'install']='xbps-install'
-F__pacmans[$__F_idx,'uninstall']='false'
-F__pacmans[$__F_idx,'update']='false'
-F__pacmans[$__F_idx,'search']='false'
-F__pacmans[$__F_idx,'show']='false'
-
-let __F_idx++
 F__pacmans[$__F_idx,'cmd']='yarn'
+F__pacmans[$__F_idx,'os']='any'
 F__pacmans[$__F_idx,'install']='yarn global add'
 F__pacmans[$__F_idx,'uninstall']='yarn remove'
 F__pacmans[$__F_idx,'update']='false'
@@ -188,6 +212,7 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='npm'
+F__pacmans[$__F_idx,'os']='any'
 F__pacmans[$__F_idx,'install']='npm install -g'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
@@ -196,7 +221,8 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='conda'
-F__pacmans[$__F_idx,'install']='conda install'
+F__pacmans[$__F_idx,'os']='any'
+F__pacmans[$__F_idx,'install']='conda install -y'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
 F__pacmans[$__F_idx,'search']='false'
@@ -204,6 +230,7 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='pip'
+F__pacmans[$__F_idx,'os']='any'
 F__pacmans[$__F_idx,'install']='pip install'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
@@ -212,6 +239,7 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='go'
+F__pacmans[$__F_idx,'os']='any'
 F__pacmans[$__F_idx,'install']='go install'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
@@ -220,6 +248,7 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='cargo'
+F__pacmans[$__F_idx,'os']='any'
 F__pacmans[$__F_idx,'install']='cargo install'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
@@ -228,6 +257,7 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='luarocks'
+F__pacmans[$__F_idx,'os']='any'
 F__pacmans[$__F_idx,'install']='luarocks install'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
@@ -236,6 +266,7 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='gem'
+F__pacmans[$__F_idx,'os']='any'
 F__pacmans[$__F_idx,'install']='gem install'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
@@ -244,12 +275,18 @@ F__pacmans[$__F_idx,'show']='false'
 
 let __F_idx++
 F__pacmans[$__F_idx,'cmd']='stack'
+F__pacmans[$__F_idx,'os']='any'
 F__pacmans[$__F_idx,'install']='stack install'
 F__pacmans[$__F_idx,'uninstall']='false'
 F__pacmans[$__F_idx,'update']='false'
 F__pacmans[$__F_idx,'search']='false'
 F__pacmans[$__F_idx,'show']='false'
 
+# Returns true if the pacman at index $1 is eligible on the current OS
+_F_pacman_os_ok() {
+	local os=${F__pacmans[$1,'os']}
+	[[ "$os" == "any" || "$os" == "$OS_KERNEL" ]]
+}
 
 F_pkg_install() {
 	for package in "$@" ; do
@@ -257,6 +294,7 @@ F_pkg_install() {
 
 		for (( i=0; i<=$__F_idx; i++ )); do
 			type $package &> /dev/null && break
+			_F_pacman_os_ok $i || continue
 
 			local pacman=${F__pacmans[$i,'cmd']}
 			local operation=${F__pacmans[$i,'install']}
@@ -279,6 +317,7 @@ F_pkg_update() {
 
 		for (( i=0; i<=$__F_idx; i++ )); do
 			[[ $($package --version) != $current_version ]] && break
+			_F_pacman_os_ok $i || continue
 
 			local pacman=${F__pacmans[$i,'cmd']}
 			local operation=${F__pacmans[$i,'update']}
@@ -296,6 +335,7 @@ F_pkg_uninstall() {
 
 		for (( i=0; i<=$__F_idx; i++ )); do
 			! type $package &> /dev/null && break
+			_F_pacman_os_ok $i || continue
 
 			local pacman=${F__pacmans[$i,'cmd']}
 			local operation=${F__pacmans[$i,'uninstall']}
@@ -311,13 +351,13 @@ F_pkg_search() {
 	for package in "$@" ; do
 		for (( i=0; i<=$__F_idx; i++ )); do
 			type $package &> /dev/null && break
+			_F_pacman_os_ok $i || continue
 
 			local pacman=${F__pacmans[$i,'cmd']}
 			local operation=${F__pacmans[$i,'search']}
 
 			if type $pacman &> /dev/null ; then
 				if eval "$operation $package" &> /tmp/hop.log ; then
-					#cat /tmp/hop.log
 					eval "$operation $package"
 					break
 				fi
@@ -332,13 +372,13 @@ F_pkg_show() {
 
 		for (( i=0; i<=$__F_idx; i++ )); do
 			type $package &> /dev/null && break
+			_F_pacman_os_ok $i || continue
 
 			local pacman=${F__pacmans[$i,'cmd']}
 			local operation=${F__pacmans[$i,'show']}
 
 			if type $pacman &> /dev/null ; then
 				if eval "$operation $package" &> /tmp/hop.log ; then
-					#cat /tmp/hop.log
 					eval "$operation $package"
 					break
 				fi
